d99cf55e4e2ca63d8e9836f39e0e4d74ceb773e8
robson <montenegrodr@gmail.com>
2018-05-26 16:34:50 +0100

    feat: add new source. us-proxy

--- letmecrawl/letmecrawl.py
+++ letmecrawl/letmecrawl.py
@@ -14,6 +14,7 @@ import pkg_resources
 
 from .sources import Source
 from .models import OrderedTable, Singleton
+from random import shuffle
 
 logger = logging.getLogger(__name__)
 
@@ -43,6 +44,8 @@ def curate():
         all_sources = []
         for s in sources:
             all_sources.extend(s.list())
+
+        shuffle(all_sources)
 
         for s in all_sources:
             table.add(s)
